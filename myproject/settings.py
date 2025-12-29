bcf862bbe5daa1bdda2b53fbb4f4174d8151f76f
issac-sjps <issac@sjps.kh.edu.tw>
2025-12-29 10:03:10 +0800

    1

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-djj&f-95hx_-#*0wazvvj7ma(g=^n8#se*8qddrpqwwx86@ybd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
